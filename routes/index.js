f2a366cdaebeecd396e2e593e312cf8f64deb122
Stoltz-Limin <s_stoltz-limin@tss-hr420-dm12.fc.ca>
2019-03-14 17:05:00 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Your first Express App', message: 'Express is actually pretty easy once you figure a few things out' });
 });
 
 module.exports = router;
